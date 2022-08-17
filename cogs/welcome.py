70fe0d1445919f779c89e3f077c5c0601c48ef0c
KillerCrazy25 <klercrazzy2mail.com>
2022-08-17 04:06:31 +0000

    Fixed stupid error

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -1,22 +1,19 @@
 import nextcord
 from nextcord.ext import commands
 
-from utils.config import WELCOME_CHANNEL_ID
+from helpers.config import WELCOME_CHANNEL_ID
 
 # Welcome Cog
-
 class Welcome(commands.Cog):
 
 	# Welcome Constructor
-
 	def __init__(self, bot : commands.Bot):
 		self.bot = bot
 
 	# Welcome Message
-
 	@commands.Cog.listener()
 	async def on_member_join(self, member : nextcord.Member):
-		welcome_channel = await self.bot.get_channel(WELCOME_CHANNEL_ID)
+		welcome_channel = self.bot.get_channel(WELCOME_CHANNEL_ID)
 
 		await welcome_channel.send(f"**{member}** has joined the server!")
 
