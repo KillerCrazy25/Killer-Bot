87039a90210fe19affec4adc4ee524a1a0e98352
KillerCrazy25 <klercrazzy2mail.com>
2022-08-19 04:00:28 +0000

    Added some constants

--- helpers/config.py
+++ helpers/config.py
@@ -20,18 +20,31 @@ VERSION = config["version"]
 PREFIX = config["prefix"]
 
 # Guild IDs
+GUILD_IDS = config["guild_ids"].values()
+
 MAIN_GUILD_ID = config["guild_ids"]["main_guild_id"]
 TESTING_GUILD_ID = config["guild_ids"]["testing_guild_id"]
 
 # User IDs
+USER_IDS = config["user_ids"].values()
+
 DEVELOPER_ID = config["user_ids"]["developer_id"]
 
 # Channel IDs
+CHANNEL_IDS = config["channel_ids"].values()
+
 ERROR_CHANNEL_ID = config["channel_ids"]["error_channel_id"]
 LOGS_CHANNEL_ID = config["channel_ids"]["logs_channel_id"]
 WELCOME_CHANNEL_ID = config["channel_ids"]["welcome_channel_id"]
 MODMAIL_CHANNEL_ID = config["channel_ids"]["modmail_channel_id"]
 DEBUG_CHANNEL_ID = config["channel_ids"]["debug_channel_id"]
+
+# Role IDs
+ROLE_IDS = config["role_ids"].values()
+
+ADMIN_ROLE_ID = config["role_ids"]["admin_role_id"]
+MOD_ROLE_ID = config["role_ids"]["mod_role_id"]
+USER_ROLE_ID = config["role_ids"]["user_role_id"]
 
 # Lavalink Things
 LAVALINK_HOST = config["lavalink"]["host"]
