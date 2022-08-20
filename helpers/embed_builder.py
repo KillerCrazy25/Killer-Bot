d8708b45eef014585f30958495f009868a9b9389
KillerCrazy25 <klercrazzy2mail.com>
2022-08-20 04:00:47 +0000

    Moved error_embed function.

--- helpers/embed_builder.py
+++ helpers/embed_builder.py
@@ -9,13 +9,31 @@ from typing import (
 
 from helpers.config import *
 from datetime import datetime
+from pytz import timezone
 
-class ModLogEmbedBuilder:
+# ModLogEmbedBuilder Class
+class EmbedBuilder:
 	
+	# ModLogEmbedBuilder Constructor
 	def __init__(self, bot : commands.Bot):
 		self.bot : commands.Bot = bot
 
-	async def build(
+	# Error Embed Builder
+	async def error_embed(self) -> nextcord.Embed:
+		"""Returns an error embed."""
+		embed = nextcord.Embed(
+			title = "Something went wrong trying to run this command.",
+			color = nextcord.Color.dark_red(),
+			timestamp = datetime.now(tz = timezone("US/Eastern"))
+		)
+
+		embed.set_author(name = "Killer Bot | Error", icon_url = self.bot.user.avatar.url)
+		embed.set_footer(text = "This message will be deleted in 10 seconds...")
+
+		return embed
+
+	# Mod Log Embed Builder
+	async def mod_log_embed(
 		self,
 		icon_url: Optional[str],
 		color: Union[nextcord.Color, int],
@@ -25,7 +43,7 @@ class ModLogEmbedBuilder:
 		timestamp: Optional[datetime] = None,
 		footer: Optional[str] = None,
 	) -> nextcord.Embed:
-		"""Generate log embed and send to logging channel."""
+		"""Returns a mod log embed."""
 		embed = nextcord.Embed(
 			description = description[:4093] + "..." if len(description) > 4096 else description
 		)
