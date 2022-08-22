49a68bf2cf023f42ca3c85f60282049e77d6c006
KillerCrazy25 <klercrazzy2mail.com>
2022-08-22 04:13:09 +0000

    Added pastebin helper.

---
+++ helpers/pastebin.py
@@ -0,0 +1,24 @@
+import requests, os
+from dotenv import load_dotenv
+from helpers.config import PASTEBIN_TOKEN
+
+load_dotenv()
+ 
+def paste(text : str, title : str):
+	url = "https://pastebin.com/api/api_post.php"
+	payload = {
+		"api_dev_key": PASTEBIN_TOKEN,
+		"api_option": "paste",
+		"api_paste_code": text,
+		"api_paste_name": title,
+		"api_paste_private": "1",
+		"api_paste_expire_date": "N",
+		"api_paste_format": "python"
+	}
+	r = requests.post(url, data = payload)
+
+	if r.status_code == 200:
+		return r.text
+	
+	else:
+		return "Error"
