745e68e5855e24c61779dd20d7ef190a374c63cc
KillerCrazy25 <klercrazzy2mail.com>
2022-08-24 04:17:01 +0000

    Added b64 function.

--- helpers/utils.py
+++ helpers/utils.py
@@ -341,6 +341,14 @@ def get_mastery_emoji(mastery : Optional[int]) -> str:
 def get_difference(dict1 : dict, dict2 : dict) -> dict:
 	return {key: dict1[key] for key in dict1 if dict1[key] != dict2[key]}
 
+# Function That Returns The Difference Between Two Lists
+def get_difference_list(list1 : list, list2 : list) -> list:
+	return [x for x in list1 if x not in list2]
+
 # Function That Chunks A String If It's Longer Than The Specified Length
 def chunk(string : str, length : int) -> list:
 	return [string[i:i+length] for i in range(0, len(string), length)]
+
+# Function that returns base64 of data parameter with padding characters to ensure its length is a multiple of 4.
+def pad_base64(data : str) -> str:	
+	return data + "=" * (-len(data) % 4)
