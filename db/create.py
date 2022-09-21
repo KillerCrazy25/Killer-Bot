3d8a2d034459929c85d1be561bbc0f09011dc166
KillerCrazy25 <klercrazzy2mail.com>
2022-09-21 01:27:48 +0000

    Removed owner_id table, added mod_log_channel_id table.

--- db/create.py
+++ db/create.py
@@ -3,6 +3,6 @@ import aiosqlite
 async def create_tables():
 	async with aiosqlite.connect("main.db") as db:
 		async with db.cursor() as cursor:
-			await cursor.execute("CREATE TABLE IF NOT EXISTS guilds (id INTEGER PRIMARY KEY, name TEXT, owner_id INTEGER)")
+			await cursor.execute("CREATE TABLE IF NOT EXISTS guilds (id INTEGER PRIMARY KEY, name TEXT, modlog_channel_id INTEGER)")
 			await cursor.execute("CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, author_id INTEGER, content TEXT, ts INTEGER)")
 		await db.commit()
