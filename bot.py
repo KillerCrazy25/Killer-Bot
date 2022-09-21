724017aeba3828090ba057a87a1542221697b448
KillerCrazy25 <klercrazzy2mail.com>
2022-09-21 03:58:01 +0000

    Help command back.

--- bot.py
+++ bot.py
@@ -1,18 +1,19 @@
 import nextcord
 import os
 import asyncio
+import traceback
 
 from nextcord.ext import commands, tasks
 
 from helpers.config import DEBUG_CHANNEL_ID, DEVELOPER_ID, MAIN_GUILD_ID, PREFIX, TESTING_GUILD_ID, TOKEN, VERSION
-from helpers.embed_builder import EmbedBuilder
+from helpers.embedder import EmbedBuilder
 from helpers.logger import Logger
 from db import *
 
 from datetime import datetime
 from pytz import timezone
 
-bot = commands.Bot(command_prefix = commands.when_mentioned_or(PREFIX), intents = nextcord.Intents.all(), help_command = None)
+bot = commands.Bot(command_prefix = commands.when_mentioned_or(PREFIX), intents = nextcord.Intents.all())
 logger = Logger()
 
 # Ready Event
@@ -22,6 +23,94 @@ async def on_ready():
 	await create_tables()
 	await change_presence_task.start()
 
+# Help View
+class HelpDropdown(nextcord.ui.View):
+
+	# Help Dropdown Constructor
+	def __init__(self):
+		super().__init__(timeout = 600)
+		self.add_item(nextcord.ui.Button(label = "Support Server", url = "https://discord.gg/3WkeV2tNas"))
+		self.add_item(nextcord.ui.Button(label = "Source Code", url = "https://github.com/KillerCrazy25/Killer-Bot"))
+
+	# Timeout Handler
+	async def on_timeout(self):
+		for child in self.children:
+			child.disabled = True
+
+		await self.message.edit(view = self)
+
+	# Select Menu
+	@nextcord.ui.select(
+		placeholder = "Choose your help page",
+		min_values = 1,
+		max_values = 1,
+		options = [
+			nextcord.SelectOption(
+				label = "Main Page",
+				description = "/help"
+			),
+			nextcord.SelectOption(
+				label = "Moderation", description = f"/help Moderation"
+			),
+			nextcord.SelectOption(
+				label = "League Of Legends", description = f"/help League Of Legends"
+			),
+			nextcord.SelectOption(
+				label = "Music", description = f"/help Music"
+			),
+			nextcord.SelectOption(
+				label = "Variety", description = f"/help Variety"
+			)
+		]
+	)
+
+	# Select Menu Callback
+	async def help_callback(self, select, interaction: nextcord.Interaction):
+		select.placeholder = f"{select.values[0]} Help Page"
+		builder = EmbedBuilder(bot)
+		if select.values[0] == "Main Page":
+			return await interaction.response.edit_message(
+				embed = await builder.help_main_page_embed(interaction.user),
+				view = self
+			)
+		embed = await builder.help_modules_page_embed(select.values[0], interaction.user)
+
+		await interaction.response.edit_message(embed = embed, view = self)
+
+# Help Command
+@bot.slash_command(name = "help", description = "Shows help message.", guild_ids = [MAIN_GUILD_ID, TESTING_GUILD_ID])
+async def help(
+	interaction : nextcord.Interaction,
+	module : str = nextcord.SlashOption(
+		name = "module",
+		description = "Show help of the given module.",
+		required = False
+	)
+):
+	if not module:
+		view = HelpDropdown()
+		builder = EmbedBuilder(bot)
+		embed = await builder.help_main_page_embed(interaction.user)
+
+		view.message = await interaction.response.send_message(embed = embed, view = view, ephemeral = True)
+	else:
+		builder = EmbedBuilder(bot)
+		embed = await builder.help_modules_page_embed(module, interaction.user)
+		view = HelpDropdown()
+
+		view.message = await interaction.response.send_message(embed = embed, view = view, ephemeral = True)
+
+@help.on_autocomplete("module")
+async def get_modules(interaction : nextcord.Interaction, module : str):
+	modules = [bot.get_cog("Moderation"), bot.get_cog("League Of Legends"), bot.get_cog("Music"), bot.get_cog("Variety")]
+	if not module:
+		await interaction.response.send_autocomplete([module.__cog_name__ for module in modules])
+		return
+	
+	await interaction.response.send_autocomplete(
+		module_list = [module.__cog_name__ for module in modules if module.__cog_name__ in module.__cog_name__]
+	)
+
 # Guilds Command
 @bot.slash_command(name = "guilds", description = "Developer only.", guild_ids = [MAIN_GUILD_ID, TESTING_GUILD_ID])
 async def guilds(interaction : nextcord.Interaction):
@@ -29,6 +118,11 @@ async def guilds(interaction : nextcord.Interaction):
 		return await interaction.send("You are not the bot developer.")
 
 	await interaction.send("I'm in " + str(len(bot.guilds)) + " guilds\n\n**Guilds**\n" + '\n'.join('Name: ' + guild.name + ' - ' + 'ID: ' + str(guild.id) for guild in bot.guilds))
+
+@bot.slash_command(name = "addguild", description = "test", guild_ids = [MAIN_GUILD_ID, TESTING_GUILD_ID])
+async def addguild(interaction: nextcord.Interaction):
+	await add_guild(interaction.guild.id, interaction.guild.name, None)
+	await interaction.send("Success!")
 
 # @bot.event
 # async def on_message(message : nextcord.Message):
@@ -172,94 +266,7 @@ for filename in os.listdir("./cogs"):
 			logger.info(f"{filename[:-3].title()} extension has been enabled.")
 		except Exception as e:
 			logger.error(f"{filename[:-3].title()} extension has failed to load!\n{e}")
-
-# Help View
-class HelpDropdown(nextcord.ui.View):
-
-	# Help Dropdown Constructor
-	def __init__(self):
-		super().__init__(timeout = 600)
-		self.add_item(nextcord.ui.Button(label = "Support Server", url = "https://discord.gg/3WkeV2tNas"))
-		self.add_item(nextcord.ui.Button(label = "Source Code", url = "https://github.com/KillerCrazy25/Killer-Bot"))
-
-	# Timeout Handler
-	async def on_timeout(self):
-		for child in self.children:
-			child.disabled = True
-
-		await self.message.edit(view = self)
-
-	# Select Menu
-	@nextcord.ui.select(
-		placeholder = "Choose your help page",
-		min_values = 1,
-		max_values = 1,
-		options = [
-			nextcord.SelectOption(
-				label = "Main Page",
-				description = "/help"
-			),
-			nextcord.SelectOption(
-				label = "Moderation", description = f"/help Moderation"
-			),
-			nextcord.SelectOption(
-				label = "League Of Legends", description = f"/help League Of Legends"
-			),
-			nextcord.SelectOption(
-				label = "Music", description = f"/help Music"
-			),
-			nextcord.SelectOption(
-				label = "Variety", description = f"/help Variety"
-			)
-		]
-	)
-
-	# Select Menu Callback
-	async def help_callback(self, select, interaction: nextcord.Interaction):
-		select.placeholder = f"{select.values[0]} Help Page"
-		builder = EmbedBuilder(bot)
-		if select.values[0] == "Main Page":
-			return await interaction.response.edit_message(
-				embed = await builder.help_main_page_embed(interaction.user),
-				view = self
-			)
-		embed = await builder.help_modules_page_embed(select.values[0], interaction.user)
-
-		await interaction.response.edit_message(embed = embed, view = self)
-
-# Help Command
-@bot.slash_command(name = "help", description = "Shows help message.", guild_ids = [MAIN_GUILD_ID, TESTING_GUILD_ID])
-async def help(
-	interaction : nextcord.Interaction,
-	module : str = nextcord.SlashOption(
-		name = "module",
-		description = "Show help of the given module.",
-		required = False
-	)
-):
-	if not module:
-		view = HelpDropdown()
-		builder = EmbedBuilder(bot)
-		embed = await builder.help_main_page_embed(interaction.user)
-
-		view.message = await interaction.response.send_message(embed = embed, view = view, ephemeral = True)
-	else:
-		builder = EmbedBuilder(bot)
-		embed = await builder.help_modules_page_embed(module, interaction.user)
-		view = HelpDropdown()
-
-		view.message = await interaction.response.send_message(embed = embed, view = view, ephemeral = True)
-
-@help.on_autocomplete("module")
-async def get_modules(interaction : nextcord.Interaction, module : str):
-	modules = [bot.get_cog("Moderation"), bot.get_cog("League Of Legends"), bot.get_cog("Music"), bot.get_cog("Variety")]
-	if not module:
-		await interaction.response.send_autocomplete([module.__cog_name__ for module in modules])
-		return
-	
-	await interaction.response.send_autocomplete(
-		module_list = [module.__cog_name__ for module in modules if module.__cog_name__ in module.__cog_name__]
-	)
+			logger.error(traceback.print_exception(e))
 
 # Run Bot
 bot.run(TOKEN)
