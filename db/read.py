99c49d0e3eec2b44094d2cba2c345eb312c3e38e
KillerCrazy25 <klercrazzy2mail.com>
2022-09-21 01:28:25 +0000

    Added get_modlog_channel and add_modlog_channel function.

--- db/read.py
+++ db/read.py
@@ -1,7 +1,15 @@
-import aiosqlite
+import aiosqlite, nextcord
 
 async def get_guild(id : int):
+    """Get guild from database."""
     async with aiosqlite.connect("main.db") as db:
         async with db.cursor() as cursor:
             await cursor.execute("SELECT * FROM guilds WHERE id = ?", (id,))
+            return await cursor.fetchone()
+
+async def get_modlog_channel(id: int):
+    """Get modlog channel for the given guild."""
+    async with aiosqlite.connect("main.db") as db:
+        async with db.cursor() as cursor:
+            await cursor.execute("SELECT modlog_channel_id FROM guilds WHERE id = ?", (id,))
             return await cursor.fetchone()
--- db/update.py
+++ db/update.py
@@ -3,11 +3,17 @@ import aiosqlite
 async def add_message(id : int, author_id : int, content : str, ts : int):
     async with aiosqlite.connect("main.db") as db:
         async with db.cursor() as cursor:
-            await cursor.execute("INSERT INTO messages VALUES (?, ?, ?, ?)", (id, author_id, content, ts))
+            await cursor.execute("INSERT INTO messages VALUES (?, ?, ?, ?)", (id, author_id, content, ts,))
         await db.commit()
 
-async def add_guild(id : int, name : str, owner_id : int):
+async def add_guild(id : int, name : str, modlog_channel_id: int):
     async with aiosqlite.connect("main.db") as db:
         async with db.cursor() as cursor:
-            await cursor.execute("INSERT INTO guilds VALUES (?, ?, ?)", (id, name, owner_id))
+            await cursor.execute("INSERT INTO guilds VALUES (?, ?, ?)", (id, name, modlog_channel_id,))
+        await db.commit()
+
+async def add_modlog_channel(guild_id: int, channel_id: int):
+    async with aiosqlite.connect("main.db") as db:
+        async with db.cursor() as cursor:
+            await cursor.execute("UPDATE guilds SET modlog_channel_id = ? WHERE id = ?", (channel_id, guild_id,))
         await db.commit()
