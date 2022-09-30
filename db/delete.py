411f84cc67796a3e30b0beb65a64ca0aa76fc2fa
KillerCrazy25 <klercrazzy2mail.com>
2022-09-30 00:12:34 +0000

    Added remove_guild function.

--- db/delete.py
+++ db/delete.py
@@ -1,7 +1,16 @@
 import aiosqlite
 
 async def delete_table(table: str):
+	"""Removes a table from the database."""
 	async with aiosqlite.connect("main.db") as db:
 		async with db.cursor() as cursor:
 			await cursor.execute(f"DROP TABLE {table}")
+		await db.commit()
+
+async def remove_guild(guild_id: int):
+	"""Removes a guild from database."""
+	async with aiosqlite.connect("main.db") as db:
+		async with db.cursor() as cursor:
+			await cursor.execute("DELETE FROM modlog WHERE guild_id = ?", (guild_id,))
+			await cursor.execute("DELETE FROM welcome WHERE guild_id = ?", (guild_id,))
 		await db.commit()
