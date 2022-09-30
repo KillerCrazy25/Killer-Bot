93f177cb02f0fd91c2e855e11b1204adf6c93396
KillerCrazy25 <klercrazzy2mail.com>
2022-09-30 00:13:11 +0000

    Added docstrings and removed annoying comments.

--- db/read.py
+++ db/read.py
@@ -1,59 +1,43 @@
 import aiosqlite
 
-#########################################################################
-#                                                                       #
-#                               MOD LOG                                 #
-#                                                                       #
-#########################################################################
-
 async def get_modlog_channel(guild_id: int):
-    """Get modlog channel for the given guild."""
-    async with aiosqlite.connect("main.db") as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("SELECT channel_id FROM modlog WHERE guild_id = ?", (guild_id,))
-            return await cursor.fetchone()
-
-#########################################################################
-#                                                                       #
-#                               WELCOME                                 #
-#                                                                       #
-#########################################################################
+	"""Get modlog channel for the given guild."""
+	async with aiosqlite.connect("main.db") as db:
+		async with db.cursor() as cursor:
+			await cursor.execute("SELECT channel_id FROM modlog WHERE guild_id = ?", (guild_id,))
+			return await cursor.fetchone()
 
 async def get_welcome_channel(guild_id: int):
-    """Get welcome channel for the given guild."""
-    async with aiosqlite.connect("main.db") as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("SELECT channel_id FROM welcome WHERE guild_id = ?", (guild_id,))
-            return await cursor.fetchone()
+	"""Get welcome channel for the given guild."""
+	async with aiosqlite.connect("main.db") as db:
+		async with db.cursor() as cursor:
+			await cursor.execute("SELECT channel_id FROM welcome WHERE guild_id = ?", (guild_id,))
+			return await cursor.fetchone()
 
 async def get_welcome_message(guild_id: int):
-    """Get welcome message for the given guild."""
-    async with aiosqlite.connect("main.db") as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("SELECT message FROM welcome WHERE guild_id = ?", (guild_id,))
-            return await cursor.fetchone()
+	"""Get welcome message for the given guild."""
+	async with aiosqlite.connect("main.db") as db:
+		async with db.cursor() as cursor:
+			await cursor.execute("SELECT message FROM welcome WHERE guild_id = ?", (guild_id,))
+			return await cursor.fetchone()
 
 async def get_welcome_role(id: int):
-    """Get welcome role for the given guild."""
-    async with aiosqlite.connect("main.db") as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("SELECT role_id FROM welcome WHERE guild_id = ?", (id,))
-            return await cursor.fetchone()
-
-#########################################################################
-#                                                                       #
-#                               ISSUES                                  #
-#                                                                       #
-#########################################################################
+	"""Get welcome role for the given guild."""
+	async with aiosqlite.connect("main.db") as db:
+		async with db.cursor() as cursor:
+			await cursor.execute("SELECT role_id FROM welcome WHERE guild_id = ?", (id,))
+			return await cursor.fetchone()
 
 async def get_issue(issue_id: str):
-    async with aiosqlite.connect("main.db") as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("SELECT * FROM issues WHERE issue_id = ?", (issue_id,))
-            return await cursor.fetchone()
+	"""Get issue data for the given issue."""
+	async with aiosqlite.connect("main.db") as db:
+		async with db.cursor() as cursor:
+			await cursor.execute("SELECT * FROM issues WHERE issue_id = ?", (issue_id,))
+			return await cursor.fetchone()
 
 async def get_all_issues(rows: int):
-    async with aiosqlite.connect("main.db") as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("SELECT * FROM issues")
-            return await cursor.fetchmany(rows)
+	"""Get the number of issues that the user wants."""
+	async with aiosqlite.connect("main.db") as db:
+		async with db.cursor() as cursor:
+			await cursor.execute("SELECT * FROM issues")
+			return await cursor.fetchmany(rows)
