d1618acc43d4b39cee3f030fff66f5ea75ce5106
KillerCrazy25 <klercrazzy2mail.com>
2022-09-30 00:11:30 +0000

    Moved some functions from update.py to create.py

--- db/create.py
+++ db/create.py
@@ -1,12 +1,25 @@
 import aiosqlite
 
-# TABLES: issues, infractions, modlog, welcome
-# await cursor.execute("CREATE TABLE IF NOT EXISTS guilds (id INTEGER PRIMARY KEY, name TEXT, modlog_channel_id INTEGER, welcome_channel_id INTEGER, welcome_message TEXT, welcome_role_id INTEGER)")
-
 async def create_tables():
+	"""Create bot tables."""
 	async with aiosqlite.connect("main.db") as db:
 		async with db.cursor() as cursor:
 			await cursor.execute("CREATE TABLE IF NOT EXISTS issues (guild_id INTEGER, user_id INTEGER, priority TEXT, status TEXT, description TEXT, issue_id TEXT)")
 			await cursor.execute("CREATE TABLE IF NOT EXISTS modlog (guild_id INTEGER, channel_id INTEGER)")
 			await cursor.execute("CREATE TABLE IF NOT EXISTS welcome (guild_id INTEGER, channel_id INTEGER, message TEXT, role_id INTEGER)")
+		await db.commit()
+
+async def create_guild(guild_id: int):
+	"""Add guild to database."""
+	async with aiosqlite.connect("main.db") as db:
+		async with db.cursor() as cursor:
+			await cursor.execute("INSERT INTO modlog VALUES (?, ?)", (guild_id, None,))
+			await cursor.execute("INSERT INTO welcome VALUES (?, ?, ?, ?)", (guild_id, None, None, None,))
+		await db.commit()
+
+async def create_issue(guild_id: int, user_id: int, priority: str, status: str, description: str, issue_id: str):
+	"""Add issue to database."""
+	async with aiosqlite.connect("main.db") as db:
+		async with db.cursor() as cursor:
+			await cursor.execute("INSERT INTO issues VALUES (?, ?, ?, ?, ?, ?)", (guild_id, user_id, priority, status, description, issue_id,))
 		await db.commit()
--- db/update.py
+++ db/update.py
@@ -1,71 +1,36 @@
 import aiosqlite
 
-#########################################################################
-#                                                                       #
-#                               GENERAL                                 #
-#                                                                       #
-#########################################################################
-
-async def add_guild(guild_id: int):
-    async with aiosqlite.connect("main.db") as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("INSERT INTO modlog VALUES (?, ?)", (guild_id, None,))
-            await cursor.execute("INSERT INTO welcome VALUES (?, ?, ?, ?)", (guild_id, None, None, None,))
-        await db.commit()
-
-#########################################################################
-#                                                                       #
-#                               MOD LOG                                 #
-#                                                                       #
-#########################################################################
-
 async def update_modlog_channel(guild_id: int, channel_id: int):
-    async with aiosqlite.connect("main.db") as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("UPDATE modlog SET channel_id = ? WHERE guild_id = ?", (channel_id, guild_id,))
-        await db.commit()
-
-#########################################################################
-#                                                                       #
-#                               WELCOME                                 #
-#                                                                       #
-#########################################################################
+	"""Update ModLog channel in the database."""
+	async with aiosqlite.connect("main.db") as db:
+		async with db.cursor() as cursor:
+			await cursor.execute("UPDATE modlog SET channel_id = ? WHERE guild_id = ?", (channel_id, guild_id,))
+		await db.commit()
 
 async def update_welcome_channel(guild_id: int, channel_id: int):
-    async with aiosqlite.connect("main.db") as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("UPDATE welcome SET channel_id = ? WHERE guild_id = ?", (channel_id, guild_id,))
-        await db.commit()
+	"""Update welcome channel in the database."""
+	async with aiosqlite.connect("main.db") as db:
+		async with db.cursor() as cursor:
+			await cursor.execute("UPDATE welcome SET channel_id = ? WHERE guild_id = ?", (channel_id, guild_id,))
+		await db.commit()
 
 async def update_welcome_message(guild_id: int, message: str):
-    async with aiosqlite.connect("main.db") as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("UPDATE welcome SET message = ? WHERE guild_id = ?", (message, guild_id,))
-        await db.commit()
+	"""Update welcome message in the database."""
+	async with aiosqlite.connect("main.db") as db:
+		async with db.cursor() as cursor:
+			await cursor.execute("UPDATE welcome SET message = ? WHERE guild_id = ?", (message, guild_id,))
+		await db.commit()
 
 async def update_welcome_role(guild_id: int, role_id: int):
-    """Update welcome role."""
-    async with aiosqlite.connect("main.db") as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("UPDATE welcome SET role_id = ? WHERE guild_id = ?", (role_id, guild_id,))
-        await db.commit()
-
-#########################################################################
-#                                                                       #
-#                               ISSUES                                  #
-#                                                                       #
-#########################################################################
-
-async def add_issue(guild_id: int, user_id: int, priority: str, status: str, description: str, issue_id: str):
-    """Add issue to database."""
-    async with aiosqlite.connect("main.db") as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("INSERT INTO issues VALUES (?, ?, ?, ?, ?, ?)", (guild_id, user_id, priority, status, description, issue_id,))
-        await db.commit()
+	"""Update welcome role in the database."""
+	async with aiosqlite.connect("main.db") as db:
+		async with db.cursor() as cursor:
+			await cursor.execute("UPDATE welcome SET role_id = ? WHERE guild_id = ?", (role_id, guild_id,))
+		await db.commit()
 
 async def update_issue(issue_id: str, status: str):
-    """Update issue status."""
-    async with aiosqlite.connect("main.db") as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("UPDATE issues SET status = ? WHERE issue_id = ?", (status, issue_id,))
-        await db.commit()
+	"""Update issue status in the database.."""
+	async with aiosqlite.connect("main.db") as db:
+		async with db.cursor() as cursor:
+			await cursor.execute("UPDATE issues SET status = ? WHERE issue_id = ?", (status, issue_id,))
+		await db.commit()
