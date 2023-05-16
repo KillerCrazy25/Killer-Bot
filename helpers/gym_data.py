0f8368b9d9991ef36e7b6db135a9fd7b99cc5619
KillerCrazy25 <79611426+KillerCrazy25@users.noreply.github.com>
2023-05-16 02:59:09 +0000

    Muscles list.

---
+++ helpers/gym_data.py
@@ -0,0 +1,18 @@
+GYM_MUSCLES = [
+    "Neck",
+    "Traps",
+    "Shoulders",
+    "Chest",
+    "Biceps",
+    "Triceps",
+    "Forearms",
+    "Obliques",
+    "Lats",
+    "Abdominals",
+    "Middle Traps",
+    "Lower Back",
+    "Glutes",
+    "Quadriceps",
+    "Hamstrings",
+    "Calves"
+]
